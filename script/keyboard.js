8387df49de6b2117a551424dd51afc96c3299f38
Valiantsin Kanapatski <Konopasies@yandex.by>
2022-05-06 22:51:57 +0300

    fix: fix keyboard.js

--- script/keyboard.js
+++ script/keyboard.js
@@ -1,70 +1,83 @@
 import {buttons} from "./lang.js"
 
-export const keyboard = document.createElement('div')
-
-keyboard.classList.add('keyboard')
-
-
-    for (let i = 0; i < 5; i++){
-        const keyboardRow = document.createElement('div')
-        keyboardRow.classList.add('keyboard__row')
-        
-            for (let k = 0; k < buttons[i].length; k++){
-                let key = document.createElement('div')
-                key.classList.add('keyboard__key')
-
-                key.dataset.code = buttons[i][k].code
-                key.dataset.letter = buttons[i][k].letter
-                key.dataset.shftE = buttons[i][k].shftE
-                key.dataset.shftR = buttons[i][k].shftR
-                key.dataset.shftEn = buttons[i][k].shftEn
-                key.dataset.shftRu = buttons[i][k].shftRu
-                key.dataset.en = buttons[i][k].en
-                key.dataset.ru = buttons[i][k].ru
-                
-                key.innerHTML = buttons[i][k].en
-                if (key.innerHTML === 'Backspace') {
-                    key.classList.add('backspace')
-                }
-                if (key.innerHTML === 'Tab' || key.innerHTML === 'Del') {
-                    key.classList.add('tab')
-                }
-                if (key.innerHTML === 'Caps Lock') {
-                    key.classList.add('caps')
-                }
-                if (key.innerHTML === 'Enter') {
-                    key.classList.add('enter')
-                }
-                if (key.innerHTML === 'Shift Left') {
-                    key.classList.add('shift_left')
-                    key.innerHTML = 'Shift'
-                }
-                if (key.innerHTML === 'Shift Right') {
-                    key.classList.add('shift_right')
-                    key.innerHTML = 'Shift'
-                }
-                if (key.innerHTML === 'Space') {
-                    key.classList.add('space')
-                    key.innerHTML = ''
-                }
-                if (key.innerHTML === 'Up') {
-                    key.innerHTML = "&#8593;";
-                }
-                if (key.innerHTML === 'Left') {
-                    key.innerHTML = "&#8592;";
-                }
-                if (key.innerHTML === 'Down') {
-                    key.innerHTML = "&#8595;";
-                }
-                if (key.innerHTML === 'Right') {
-                    key.innerHTML = "&#8594;";
-                }
-                keyboardRow.append(key)
-            }
-        
-    
-        keyboard.append(keyboardRow)
+class Keyboard {
+    constructor() {
+        this.keyboard
+        this.keyboardRow
+        this.key
     }
+    createKeyboard(buttons) {
+        this.keyboard = document.createElement('div')
+        this.keyboard.classList.add('keyboard')
+        this.createKeyboardRow(buttons)
+        return this.keyboard
+    }
+    createKeyboardRow(buttons) {
+        for (let i = 0; i < buttons.length; i++){
+            this.keyboardRow = document.createElement('div')
+            this.keyboardRow.classList.add('keyboard__row')
+            this.createKeyButton(buttons[i])     
+            this.keyboard.append(this.keyboardRow)
+        }
+    }
+    createKeyButton (keyboradRow) {
+        for (let k = 0; k < keyboradRow.length; k++){
+            this.key = document.createElement('div')
+            this.key.classList.add('keyboard__key')
+
+            this.key.dataset.code = keyboradRow[k].code
+            this.key.dataset.letter = keyboradRow[k].letter
+            this.key.dataset.shftE = keyboradRow[k].shftE
+            this.key.dataset.shftR = keyboradRow[k].shftR
+            this.key.dataset.shftEn = keyboradRow[k].shftEn
+            this.key.dataset.shftRu = keyboradRow[k].shftRu
+            this.key.dataset.en = keyboradRow[k].en
+            this.key.dataset.ru = keyboradRow[k].ru
+            
+            this.key.innerHTML = keyboradRow[k].en
+            if (this.key.innerHTML === 'Backspace') {
+                this.key.classList.add('backspace')
+            }
+            if (this.key.innerHTML === 'Tab' || this.key.innerHTML === 'Del') {
+                this.key.classList.add('tab')
+            }
+            if (this.key.innerHTML === 'Caps Lock') {
+                this.key.classList.add('caps')
+            }
+            if (this.key.innerHTML === 'Enter') {
+                this.key.classList.add('enter')
+            }
+            if (this.key.innerHTML === 'Shift Left') {
+                this.key.classList.add('shift_left')
+                this.key.innerHTML = 'Shift'
+            }
+            if (this.key.innerHTML === 'Shift Right') {
+                this.key.classList.add('shift_right')
+                this.key.innerHTML = 'Shift'
+            }
+            if (this.key.innerHTML === 'Space') {
+                this.key.classList.add('space')
+                this.key.innerHTML = ''
+            }
+            if (this.key.innerHTML === 'Up') {
+                this.key.innerHTML = "&#8593;";
+            }
+            if (this.key.innerHTML === 'Left') {
+                this.key.innerHTML = "&#8592;";
+            }
+            if (this.key.innerHTML === 'Down') {
+                this.key.innerHTML = "&#8595;";
+            }
+            if (this.key.innerHTML === 'Right') {
+                this.key.innerHTML = "&#8594;";
+            }
+            this.keyboardRow.append(this.key)
+        }
+    }
+}
+
+let board = new Keyboard() 
+export const keyboard = board.createKeyboard(buttons)
 
 
 
